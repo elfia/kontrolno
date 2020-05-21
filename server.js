c12290f5a1701ff7bb4e01d9577881fdc259019c
Elfi Hristova <ehristova@botronsoft.com>
2020-05-21 12:51:33 +0300

    remove domashno stuff

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const fs = require('fs');
 
 const spisak = {};
 const kontrolnoBlanki = [];
-const domashnoBlanki = [];
 let current = 0;
 
 fs.readdirSync('blanki/').forEach((file) => {
@@ -16,14 +15,7 @@ fs.readdirSync('blanki/').forEach((file) => {
   }
 });
 
-fs.readdirSync('domashno/').forEach((file) => {
-  if (file !== '.DS_Store') {
-    domashnoBlanki.push(file);
-  }
-});
-
 console.log('Kontrolni: ', kontrolnoBlanki);
-console.log('Domashni: ', domashnoBlanki);
 
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
@@ -35,7 +27,6 @@ app.get('/', (req, res) => {
 app.use('/static/css', express.static(path.join(__dirname, '/build/static/css')));
 app.use('/static/js', express.static(path.join(__dirname, '/build/static/js')));
 app.use('/blanki', express.static(path.join(__dirname, '/blanki')));
-app.use('/domashno', express.static(path.join(__dirname, '/domashno')));
 
 app.get('/start', function (req, res) {
   const newEntry = req.query.klass + '_' + req.query.ime + '_' + req.query.nomer;
@@ -73,35 +64,6 @@ app.get('/start', function (req, res) {
   // });
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('/blanki/' + spisak[newEntry]);
-  res.end();
-});
-
-app.get('/domashno', function (req, res) {
-  const newEntry = req.query.klass + '_' + req.query.ime + '_' + req.query.nomer;
-  if (!spisak[newEntry]) {
-    spisak[newEntry] = domashnoBlanki[current];
-    current++;
-    console.log('НОВ УЧЕНИК: ' + newEntry + ' със задание: ' + spisak[newEntry]);
-    if (current == domashnoBlanki.length) {
-      current = 0;
-    }
-  } else {
-    console.log('Повторен опит за дете: ' + newEntry + ' със задание: ' + spisak[newEntry]);
-  }
-  console.log(spisak);
-  // fs.appendFile('tests/' + newEntry + '.txt', spisak[newEntry], function (err) {
-  //   if (err) {
-  //     console.log(err);
-  //     delete spisak[newEntry];
-  //     res.status(400).send({
-  //       message: 'Не можахме да те запишем',
-  //     });
-  //     return;
-  //   }
-  // });
-
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('/domashno/' + spisak[newEntry]);
   res.end();
 });
 
