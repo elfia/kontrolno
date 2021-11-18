6f878e0da30b732eaa79be62e47484b58a738684
elfia <ehristova@botronsoft.com>
2021-11-18 16:18:27 +0200

    fix class

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import React from 'react';
-import './App.css';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import React from "react";
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 function App() {
-  const [name, setName] = React.useState('');
-  const [classNumber, setClassNumber] = React.useState('');
-  const [error, setError] = React.useState('');
-  const [number, setNumber] = React.useState('');
+  const [name, setName] = React.useState("");
+  const [classNumber, setClassNumber] = React.useState("");
+  const [error, setError] = React.useState("");
+  const [number, setNumber] = React.useState("");
   const [downloadUrl, setDownloadUrl] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(false);
 
   const submit = async () => {
     if (name.length < 2) {
-      setError('Въведете първото си име');
+      setError("Въведете първото си име");
       return;
     }
-    if (name.trim().split(' ').length > 1) {
-      setError('Въведете само първото си име без фамилия');
+    if (name.trim().split(" ").length > 1) {
+      setError("Въведете само първото си име без фамилия");
       return;
     }
     if (classNumber.length < 1) {
-      setError('Изберете клас');
+      setError("Изберете клас");
       return;
     }
     if (number.length < 1) {
-      setError('Въведете правилно номера в класа');
+      setError("Въведете правилно номера в класа");
       return;
     }
     setIsLoading(true);
-    setError('');
-    const response = await fetch('/start?ime=' + name + '&nomer=' + number + '&klass=' + classNumber, {
-      method: 'GET', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+    setError("");
+    const response = await fetch("/start?ime=" + name + "&nomer=" + number + "&klass=" + classNumber, {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *client
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *client
     });
     if (response.status >= 400) {
-      setError('Нещо се обърка, пробвай пак. Ако видиш това съобщение отново пиши на г-жата');
+      setError("Нещо се обърка, пробвай пак. Ако видиш това съобщение отново пиши на г-жата");
     } else {
       const text = await response.text();
       window.location.href = text;
@@ -67,12 +67,12 @@ function App() {
             variant="outlined"
             value={name}
             fullWidth
-            error={error == 'Въведете първото си име' || error == 'Въведете само първото си име без фамилия'}
+            error={error == "Въведете първото си име" || error == "Въведете само първото си име без фамилия"}
             onInput={(e) => setName(e.target.value)}
           />
         </p>
         <p>
-          <FormControl variant="outlined" error={error == 'Изберете клас'}>
+          <FormControl variant="outlined" error={error == "Изберете клас"}>
             <InputLabel id="demo-simple-select-filled-label">Изберете Клас</InputLabel>
             <Select
               autoWidth
@@ -84,7 +84,7 @@ function App() {
               }}
               label="Изберете Клас"
             >
-              <MenuItem value="8б">8б</MenuItem>
+              <MenuItem value="9a">9a</MenuItem>
             </Select>
           </FormControl>
         </p>
@@ -97,7 +97,7 @@ function App() {
             fullWidth
             value={number}
             type="number"
-            error={error == 'Въведете правилно номера в класа'}
+            error={error == "Въведете правилно номера в класа"}
             onInput={(e) => setNumber(e.target.value)}
           />
         </p>
